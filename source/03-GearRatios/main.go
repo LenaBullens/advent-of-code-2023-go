7880d2d7e39ac2e14b91c441c0ebceb552689140
LenaBullens <BullensLena@gmail.com>
2023-12-04 00:35:11 +0100

    day 3 part 1

--- source/03-GearRatios/main.go
+++ source/03-GearRatios/main.go
@@ -1,5 +1,115 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"unicode"
 
+	"github.com/LenaBullens/advent-of-code-2022-go/source/helper"
+)
+
+type Point struct {
+	x int
+	y int
+}
+
+func main() {
+	part1()
+}
+
+func part1() {
+	grid := helper.ReadGrid("input.txt")
+	yMax := len(grid) - 1
+	xMax := len(grid[0]) - 1
+
+	symbolZoneMap := make(map[Point]bool)
+	for y, row := range grid {
+		for x, tile := range row {
+			if isSymbol(tile) {
+				symbolZoneMap[Point{x, y}] = true
+				if x > 0 && y > 0 {
+					symbolZoneMap[Point{x - 1, y - 1}] = true
+				}
+				if y > 0 {
+					symbolZoneMap[Point{x, y - 1}] = true
+				}
+				if x < xMax && y > 0 {
+					symbolZoneMap[Point{x + 1, y - 1}] = true
+				}
+				if x > 0 {
+					symbolZoneMap[Point{x - 1, y}] = true
+				}
+				if x < xMax {
+					symbolZoneMap[Point{x + 1, y}] = true
+				}
+				if x > 0 && y < yMax {
+					symbolZoneMap[Point{x - 1, y + 1}] = true
+				}
+				if y < yMax {
+					symbolZoneMap[Point{x, y + 1}] = true
+				}
+				if x < xMax && y < yMax {
+					symbolZoneMap[Point{x + 1, y + 1}] = true
+				}
+			}
+		}
+	}
+
+	sum := 0
+
+	for y, row := range grid {
+		currentNumberString := ""
+		isPart := false
+		for x, tile := range row {
+			if isNumber(tile) {
+				currentNumberString = currentNumberString + tile
+				if symbolZoneMap[Point{x, y}] {
+					isPart = true
+				}
+			} else {
+				//End of number
+				if currentNumberString != "" {
+					number, err := strconv.Atoi(currentNumberString)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if isPart {
+						sum = sum + number
+					}
+					currentNumberString = ""
+					isPart = false
+				}
+			}
+			if x == xMax {
+				//End of row
+				if currentNumberString != "" {
+					number, err := strconv.Atoi(currentNumberString)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if isPart {
+						sum = sum + number
+					}
+					currentNumberString = ""
+					isPart = false
+				}
+			}
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isSymbol(input string) bool {
+	if input == "." {
+		return false
+	}
+	runes := []rune(input)
+	return !unicode.IsDigit(runes[0])
+}
+
+func isNumber(input string) bool {
+	runes := []rune(input)
+	return unicode.IsDigit(runes[0])
 }
--- source/helper/helper.go
+++ source/helper/helper.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 func ReadLines(path string) []string {
@@ -20,9 +21,34 @@ func ReadLines(path string) []string {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
 	return lines
+}
+
+func ReadGrid(path string) [][]string {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var grid [][]string
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		rowArray := strings.Split(scanner.Text(), "")
+		grid = append(grid, rowArray)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return grid
 }
