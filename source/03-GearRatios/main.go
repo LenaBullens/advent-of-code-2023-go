bc4115ab28724230ea63c0c78db50df95882bc8d
LenaBullens <BullensLena@gmail.com>
2023-12-04 01:17:40 +0100

    day 3 part 2

--- source/03-GearRatios/main.go
+++ source/03-GearRatios/main.go
@@ -10,12 +10,12 @@ import (
 )
 
 type Point struct {
-	x int
-	y int
+	X int
+	Y int
 }
 
 func main() {
-	part1()
+	part2()
 }
 
 func part1() {
@@ -68,7 +68,7 @@ func part1() {
 					isPart = true
 				}
 			} else {
-				//End of number
+				// End of number
 				if currentNumberString != "" {
 					number, err := strconv.Atoi(currentNumberString)
 					if err != nil {
@@ -82,7 +82,7 @@ func part1() {
 				}
 			}
 			if x == xMax {
-				//End of row
+				// End of row
 				if currentNumberString != "" {
 					number, err := strconv.Atoi(currentNumberString)
 					if err != nil {
@@ -112,4 +112,108 @@ func isSymbol(input string) bool {
 func isNumber(input string) bool {
 	runes := []rune(input)
 	return unicode.IsDigit(runes[0])
+}
+
+type PartNumber struct {
+	Value int
+	Start Point
+	End   Point
+}
+
+func (pn PartNumber) isNeighbour(point Point) bool {
+	if (point.X >= pn.Start.X-1) && (point.X <= pn.End.X+1) {
+		if (point.Y >= pn.Start.Y-1) && (point.Y <= pn.Start.Y+1) { // Start.Y == End.Y
+			return true
+		}
+	}
+	return false
+}
+
+func part2() {
+	grid := helper.ReadGrid("input.txt")
+	xMax := len(grid[0]) - 1
+
+	gearMap := make(map[Point]bool)
+	for y, row := range grid {
+		for x, tile := range row {
+			if isGear(tile) {
+				gearMap[Point{x, y}] = true
+			}
+		}
+	}
+
+	var partNumbers []PartNumber
+	for y, row := range grid {
+		start := Point{}
+		end := Point{}
+		currentNumberString := ""
+		for x, tile := range row {
+			if isNumber(tile) {
+				// Start of new number
+				if currentNumberString == "" {
+					start.X = x
+					start.Y = y
+				}
+				currentNumberString = currentNumberString + tile
+			} else {
+				// End of number
+				if currentNumberString != "" {
+					number, err := strconv.Atoi(currentNumberString)
+					if err != nil {
+						log.Fatal(err)
+					}
+					end.X = x - 1
+					end.Y = y
+					pn := PartNumber{
+						Value: number,
+						Start: start,
+						End:   end,
+					}
+					partNumbers = append(partNumbers, pn)
+					currentNumberString = ""
+				}
+			}
+			if x == xMax {
+				// End of row
+				if currentNumberString != "" {
+					number, err := strconv.Atoi(currentNumberString)
+					if err != nil {
+						log.Fatal(err)
+					}
+					end.X = x
+					end.Y = y
+					pn := PartNumber{
+						Value: number,
+						Start: start,
+						End:   end,
+					}
+					partNumbers = append(partNumbers, pn)
+					currentNumberString = ""
+				}
+			}
+		}
+	}
+
+	sum := 0
+
+	for gear := range gearMap {
+		nbOfNeighbours := 0
+		var neighbours []PartNumber
+		for _, pn := range partNumbers {
+			if pn.isNeighbour(gear) {
+				nbOfNeighbours++
+				neighbours = append(neighbours, pn)
+			}
+		}
+		if nbOfNeighbours == 2 {
+			gearRatio := neighbours[0].Value * neighbours[1].Value
+			sum = sum + gearRatio
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isGear(input string) bool {
+	return input == "*"
 }
