91e9c14553e2bde35a4b0aa788437d3f43837cd9
LenaBullens <BullensLena@gmail.com>
2023-12-06 00:15:43 +0100

    day 5 part 2

--- source/05-IfYouGiveASeedAFertilizer/main.go
+++ source/05-IfYouGiveASeedAFertilizer/main.go
@@ -25,6 +25,11 @@ type Mapping struct {
 }
 
 func main() {
+	//part1()
+	part2()
+}
+
+func part1() {
 	lines := helper.ReadLines("input.txt")
 
 	// Seeds
@@ -231,6 +236,224 @@ func main() {
 	fmt.Println(lowest)
 }
 
+func part2() {
+	lines := helper.ReadLines("input.txt")
+
+	// Seeds
+	seedsLine := lines[0]
+	seedNumbersString := seedsLine[7:]
+	splitString1 := strings.Split(seedNumbersString, " ")
+	var seedRanges []Range
+	for i := 0; i < len(splitString1); i = i + 2 {
+		seedStart := parseInt(splitString1[i])
+		offset := parseInt(splitString1[i+1])
+		r := Range{
+			Start: seedStart,
+			End:   seedStart + offset - 1,
+		}
+		seedRanges = append(seedRanges, r)
+	}
+
+	// Seeds to soil
+	i := 3
+	var seedToSoilMappings []Mapping
+	for lines[i] != "" {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		seedToSoilMappings = append(seedToSoilMappings, mapping)
+		i++
+	}
+
+	// Soil to fertilizer
+	i = i + 2
+	var soilToFertilizerMappings []Mapping
+	for lines[i] != "" {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		soilToFertilizerMappings = append(soilToFertilizerMappings, mapping)
+		i++
+	}
+
+	// Fertilizer to water
+	i = i + 2
+	var fertilizerToWaterMappings []Mapping
+	for lines[i] != "" {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		fertilizerToWaterMappings = append(fertilizerToWaterMappings, mapping)
+		i++
+	}
+
+	// Water to light
+	i = i + 2
+	var waterToLightMappings []Mapping
+	for lines[i] != "" {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		waterToLightMappings = append(waterToLightMappings, mapping)
+		i++
+	}
+
+	// Light to temperature
+	i = i + 2
+	var lightToTemperatureMappings []Mapping
+	for lines[i] != "" {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		lightToTemperatureMappings = append(lightToTemperatureMappings, mapping)
+		i++
+	}
+
+	// Temperature to humidity
+	i = i + 2
+	var temperatureToHumidityMappings []Mapping
+	for lines[i] != "" {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		temperatureToHumidityMappings = append(temperatureToHumidityMappings, mapping)
+		i++
+	}
+
+	// Humidity to location
+	i = i + 2
+	var humidityToLocationMappings []Mapping
+	for i < len(lines) {
+		splitString2 := strings.Split(lines[i], " ")
+		destinationStart := parseInt(splitString2[0])
+		sourceStart := parseInt(splitString2[1])
+		width := parseInt(splitString2[2])
+
+		source := Range{
+			Start: sourceStart,
+			End:   sourceStart + width - 1,
+		}
+		destination := Range{
+			Start: destinationStart,
+			End:   destinationStart + width - 1,
+		}
+		mapping := Mapping{
+			Source:      source,
+			Destination: destination,
+		}
+		humidityToLocationMappings = append(humidityToLocationMappings, mapping)
+		i++
+	}
+
+	done := false
+	location := 0
+	for !done {
+		humidityNumber := doReverseMap(location, humidityToLocationMappings)
+		temperatureNumber := doReverseMap(humidityNumber, temperatureToHumidityMappings)
+		lightNumber := doReverseMap(temperatureNumber, lightToTemperatureMappings)
+		waterNumber := doReverseMap(lightNumber, waterToLightMappings)
+		fertilizerNumber := doReverseMap(waterNumber, fertilizerToWaterMappings)
+		soilNumber := doReverseMap(fertilizerNumber, soilToFertilizerMappings)
+		seedNumber := doReverseMap(soilNumber, seedToSoilMappings)
+
+		for _, seedRange := range seedRanges {
+			if seedRange.Contains(seedNumber) {
+				done = true
+			}
+		}
+
+		if !done {
+			location++
+		}
+	}
+
+	fmt.Println(location)
+}
+
 func parseInt(input string) int {
 	result, err := strconv.Atoi(input)
 	if err != nil {
@@ -246,6 +469,24 @@ func doMap(input int, mappings []Mapping) int {
 		if mapping.Source.Contains(input) {
 			offset := input - mapping.Source.Start
 			result = mapping.Destination.Start + offset
+			done = true
+			break
+		}
+	}
+	if !done {
+		result = input
+	}
+
+	return result
+}
+
+func doReverseMap(input int, mappings []Mapping) int {
+	done := false
+	var result int
+	for _, mapping := range mappings {
+		if mapping.Destination.Contains(input) {
+			offset := input - mapping.Destination.Start
+			result = mapping.Source.Start + offset
 			done = true
 			break
 		}
